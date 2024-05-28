b393e3dd371947af003dcd596e40f54adc449f26
Everton Reis <evertonstm@gmail.com>
2024-05-28 18:59:42 -0400

    adicionado aula 2

--- imc.py
+++ imc.py
@@ -1,35 +1,23 @@
-def calcular_imc(peso, altura):
-    """
-    Calcula o Índice de Massa Corporal (IMC) com base no peso (em kg) e altura (em metros).
-    Retorna o valor do IMC.
-    """
-    imc = peso / (altura ** 2)
-    return imc
+# Solicitar ao usuário que insira o peso e a altura
+peso = float(input("Digite o peso (em kg): "))
+altura = float(input("Digite a altura (em metros): "))
 
-def classificar_imc(imc):
-    """
-    Classifica o IMC em uma faixa específica e retorna uma mensagem correspondente.
-    """
-    if imc < 18.5:
-        return "Abaixo do peso"
-    elif 18.5 <= imc < 25:
-        return "Peso normal"
-    elif 25 <= imc < 30:
-        return "Sobrepeso"
-    elif 30 <= imc < 35:
-        return "Obesidade grau 1"
-    elif 35 <= imc < 40:
-        return "Obesidade grau 2"
-    else:
-        return "Obesidade grau 3"
+# Calcular o IMC
+imc = peso / (altura ** 2)
 
-def main():
-    peso = float(input("Digite seu peso (em kg): "))
-    altura = float(input("Digite sua altura (em metros): "))
+# Imprimir o IMC
+print("Seu IMC é:", imc)
 
-    imc = calcular_imc(peso, altura)
-    print("Seu IMC é:", imc)
-    print("Você está na faixa de:", classificar_imc(imc))
-
-if __name__ == "__main__":
-    main()
+# Determinar e imprimir a faixa do IMC
+if imc < 18.5:
+    print("Você está abaixo do peso.")
+elif imc < 25:
+    print("Seu peso está normal.")
+elif imc < 30:
+    print("Você está com sobrepeso.")
+elif imc < 35:
+    print("Você está com obesidade Grau I.")
+elif imc < 40:
+    print("Você está com obesidade Grau II (severa).")
+else:
+    print("Você está com obesidade Grau III (mórbida).")
